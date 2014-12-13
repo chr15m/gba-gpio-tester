1ef59c88e543993a8039dbed870e003474eb826c
Chris McCormick <chris@mccormick.cx>
2014-12-13 20:42:01 +0800

    Got all pin outputs working with different buttons to toggle.

--- source/main.c
+++ source/main.c
@@ -8,7 +8,11 @@
 
 #include <stdio.h>
 
-#define STATE(x) ((bool)x ? "on" : "off")
+#define STATE(x) ((bool)x ? "on " : "off")
+
+void redraw(int line, char id, char keyname, char* state) {
+	iprintf("\x1b[%d;3HS%c (%c): %s", line, id, keyname, state);
+}
 
 //---------------------------------------------------------------------------------
 // Program entry point
@@ -25,14 +29,21 @@ int main(void) {
 	// set the base GPIO flags
 	REG_RCNT = R_GPIO | GPIO_SC_IO | GPIO_SD_IO | GPIO_SI_IO | GPIO_SO_IO;
 	
+	iprintf("\x1b[2J");
+	iprintf("\x1b[1;1HGPIO Tester");
+	iprintf("\x1b[2;1H-----------");
+	redraw(4, 'O', 'A', STATE(REG_RCNT & GPIO_SO));
+	redraw(5, 'I', 'B', STATE(REG_RCNT & GPIO_SI));
+	redraw(6, 'C', 'L', STATE(REG_RCNT & GPIO_SC));
+	redraw(7, 'D', 'R', STATE(REG_RCNT & GPIO_SD));
+	
 	while (1) {
 		VBlankIntrWait();
-		iprintf("\x1b[2J");
-		iprintf("\x1b[1;1HGPIO Tester");
-		iprintf("\x1b[2;1H-----------");
-		// SO
-		iprintf("\x1b[4;3HSO (A): %s", STATE(REG_RCNT & GPIO_SO));
-
+		//for (i=0; i<27; i++) {
+		//	iprintf("\x1b[%d;0H*", i);
+		//}
+		
+		// while(REG_VCOUNT != 160);
 		scanKeys();
 
 		keys_pressed = keysDown();
@@ -42,6 +53,28 @@ int main(void) {
 		if ( keys_pressed & KEY_A ) {
 			// toggle
 			REG_RCNT ^= GPIO_SO;
+			redraw(4, 'O', 'A', STATE(REG_RCNT & GPIO_SO));
+		}
+
+		// turn on/off GPIOs.
+		if ( keys_pressed & KEY_B ) {
+			// toggle
+			REG_RCNT ^= GPIO_SI;
+			redraw(5, 'I', 'B', STATE(REG_RCNT & GPIO_SI));
+		}
+
+		// turn on/off GPIOs.
+		if ( keys_pressed & KEY_L ) {
+			// toggle
+			REG_RCNT ^= GPIO_SC;
+			redraw(6, 'C', 'L', STATE(REG_RCNT & GPIO_SC));
+		}
+
+		// turn on/off GPIOs.
+		if ( keys_pressed & KEY_R ) {
+			// toggle
+			REG_RCNT ^= GPIO_SD;
+			redraw(7, 'D', 'R', STATE(REG_RCNT & GPIO_SD));
 		}
 
 		/*if ( keys_released & KEY_A ) {
@@ -50,4 +83,3 @@ int main(void) {
 		
 	}
 }
-
