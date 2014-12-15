5f65ed45d130004e0f5bbbdf0246aa631a42127f
Chris McCormick <chris@mccormick.cx>
2014-12-15 20:59:36 +0800

    Code to test timer 0 with GPIOs too.

--- source/main.c
+++ source/main.c
@@ -2,9 +2,11 @@
 #include <gba_console.h>
 #include <gba_video.h>
 #include <gba_interrupt.h>
+#include <gba_timers.h>
 #include <gba_systemcalls.h>
 #include <gba_input.h>
 #include <gba_sio.h>
+#include <mappy.h>
 
 #include <stdio.h>
 
@@ -12,6 +14,22 @@
 
 void redraw(int line, char id, char keyname, char* state) {
 	iprintf("\x1b[%d;3HS%c (%c): %s", line, id, keyname, state);
+}
+
+void redrawTimer(char *state) {
+	iprintf("\x1b[9;1HTimer 0 ISR (START): %s", state);
+}
+
+#define IWRAM_CODE	__attribute__((section(".iwram"), long_call))
+// void InterruptProcess(void) __attribute__ ((section(".iwram")));   // the ISR must be declared in this way
+inline void timerInterrupt(void) IWRAM_CODE;
+
+inline void timerInterrupt(void) {
+	// clear interrupt flag
+	// dprintf("Hi!\n");
+	REG_RCNT ^= GPIO_SC;
+	redraw(6, 'C', 'L', STATE(REG_RCNT & GPIO_SC));
+	REG_IF |= REG_IF;
 }
 
 //---------------------------------------------------------------------------------
@@ -24,10 +42,23 @@ int main(void) {
 	irqInit();
 	irqEnable(IRQ_VBLANK);
 	
-	consoleDemoInit();
-	
 	// set the base GPIO flags
 	REG_RCNT = R_GPIO | GPIO_SC_IO | GPIO_SD_IO | GPIO_SI_IO | GPIO_SO_IO;
+	
+	irqSet( IRQ_TIMER0, timerInterrupt );
+	irqEnable( IRQ_TIMER0 );
+	
+	// turn off timer 0
+	REG_TM0CNT_H = 0;
+	// setup timer 0
+	// 65536 - 64999 = 537
+	// (16 * 1024 * 1024) / 537 = 31242 bps
+	// MIDI is 31250 bsp at +/-1% tolerance (-1% => 30937, +1% => 31562)
+	// so 31242 bps is a valid bps value
+	REG_TM0CNT_L = 0xFDE7;   // 64999
+	REG_TM0CNT_H = TIMER_START | TIMER_IRQ | 0x3;   // start + irq + f (16MHz)
+	
+	consoleDemoInit();
 	
 	iprintf("\x1b[2J");
 	iprintf("\x1b[1;1HGPIO Tester");
@@ -36,6 +67,8 @@ int main(void) {
 	redraw(5, 'I', 'B', STATE(REG_RCNT & GPIO_SI));
 	redraw(6, 'C', 'L', STATE(REG_RCNT & GPIO_SC));
 	redraw(7, 'D', 'R', STATE(REG_RCNT & GPIO_SD));
+	redrawTimer(STATE(REG_TM0CNT_H & TIMER_START));
+	dprintf("Hi!\n");
 	
 	while (1) {
 		VBlankIntrWait();
@@ -75,6 +108,13 @@ int main(void) {
 			// toggle
 			REG_RCNT ^= GPIO_SD;
 			redraw(7, 'D', 'R', STATE(REG_RCNT & GPIO_SD));
+		}
+
+		// turn on/off 
+		if ( keys_pressed & KEY_START ) {
+			// toggle
+			REG_TM0CNT_H ^= TIMER_START;
+			redrawTimer(STATE(REG_TM0CNT_H & TIMER_START));
 		}
 
 		/*if ( keys_released & KEY_A ) {
